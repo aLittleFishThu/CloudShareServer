1dca5c03a9c756f5a69f1ea4ad84972420baf2a8
yzj <yuzhujun14@hotmail.com>
2017-04-24 22:11:46 +0800

    test OK, client/server, upload and Usermanager, 用户管理和上传文件均正常，中文有乱码

--- src/test/TestDAL.java
+++ src/test/TestDAL.java
@@ -16,14 +16,16 @@ public class TestDAL {
 		dataAccess.storeUserSheet(sheet);
 		HashMap<String,User> sheet2=dataAccess.getUserSheet();
 		System.out.println(sheet2);*/
-        HashMap<String,HashSet<CloudFile>> sheet=new HashMap<String,HashSet<CloudFile>>();
+        /*HashMap<String,HashSet<CloudFile>> sheet=new HashMap<String,HashSet<CloudFile>>();
         CloudFile cloud1=new CloudFile("yzj","123");
         CloudFile cloud2=new CloudFile("yzj2","123");
         HashSet<CloudFile> set=new HashSet<CloudFile>();
         set.add(cloud1);set.add(cloud2);
         sheet.put("1", set);
-        dataAccess.storeUserFileSheet(sheet);
+        dataAccess.storeUserFileSheet(sheet);*/
         HashMap<String,HashSet<CloudFile>> sheet2=dataAccess.getUserFileSheet();
         System.out.println(sheet2);
+        HashMap<String,CloudFile> sheet3=dataAccess.getFileSheet();
+        System.out.println(sheet3);
 	}
 }
